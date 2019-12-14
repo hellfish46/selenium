0a65d24c29237b61e5531a5e3e64cdc385dc485a
Anton Gritsun <hellfish46@i.ua>
2019-12-15 01:12:26 +0200

    maven project

---
+++ src/test/java/com/automation/TestOne.java
@@ -0,0 +1,21 @@
+package com.automation;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestOne {
+
+
+    @Test
+    public void test1() {
+        int a = 2;
+        int b = 2;
+        int result = a + b;
+        System.out.println("11111111111111111111111111111111111111");
+        assertEquals(result,4 );
+
+
+
+
+    }
+}
